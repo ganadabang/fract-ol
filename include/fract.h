4e6bf80817c765f56c1c523ebcc7596c90a995d3
hyeonsok <hyeonsok@student.42seoul.kr>
2021-07-31 00:53:41 +0900

    Add keycode, button value

--- include/fract.h
+++ include/fract.h
@@ -8,9 +8,18 @@
 #include <math.h>
 #include <stdio.h>
 
-#define X 600
-#define Y 400
-
+//size
+# define X 1280
+# define Y 720
+//button
+# define WHEELUP 4
+# define WHEELDOWN 5
+//keycode
+# define ESC 53
+# define LEFT 123
+# define RIGHT 124
+# define DOWN 125
+# define UP 126
 
 typedef struct s_data {
 	void	*img;
@@ -37,7 +46,7 @@ void	ft_mlx_pixel_put(t_data *data, int x, int y, int color);
 int		create_trgb(int t, int r, int g, int b);
 void	set_mlx_img(int x, int y, void *mlx, t_data *img);
 
-int		*setbound(int start1, int stop1, int start2, int stop2);
-int		map(int	value, int *bound);
+double	*setbound(double start1, double stop1, double start2, double stop2);
+double	map(double value, double *bound);
 
 #endif
